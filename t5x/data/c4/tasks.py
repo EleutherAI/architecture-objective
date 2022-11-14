e981b7c7e64d9b52f982bd3aa14e16f876f30823
lintangsutawika <lintang@sutawika.com>
2022-11-14 16:07:52 +0000

    added fcm style dataset for c4

--- t5x/data/c4/tasks.py
+++ t5x/data/c4/tasks.py
@@ -15,8 +15,9 @@ For more details, see: seqio/scripts/cache_tasks_main.py
 import seqio
 import functools
 
-from t5.data import preprocessors
+import t5.data.preprocessors
 
+import t5x.data.preprocessors
 from t5x.data.vocab import DEFAULT_OUTPUT_FEATURES
 from t5x.data.utils import CustomDataSource, extract_text_from_json_tf
 
@@ -40,9 +41,40 @@ TaskRegistry.add(
             }),
         seqio.preprocessors.tokenize,
         seqio.CacheDatasetPlaceholder(),
-        preprocessors.span_corruption,
+        t5.data.preprocessors.span_corruption,
         seqio.preprocessors.append_eos_after_trim,
     ],
     output_features=DEFAULT_OUTPUT_FEATURES,
+    metric_fns=[]
+)
+
+TaskRegistry.add(
+    'c4_fcm_causal_decoder_architecture',
+    source=CustomDataSource(
+        split_to_filepattern=c4_utils.get_c4_files(),
+    ),
+    preprocessors=[
+        extract_text_from_json_tf,
+        functools.partial(
+            seqio.preprocessors.rekey, key_map={
+                "inputs": None,
+                "targets": "text"
+            }),
+        seqio.preprocessors.tokenize,
+        seqio.CacheDatasetPlaceholder(),
+        t5x.data.preprocessors.masked_language_modeling,
+        seqio.preprocessors.append_eos_after_trim,
+        t5x.data.preprocessors.pack_lm_decoder_only,
+    ],
+    output_features={
+        "decoder_target_tokens": seqio.Feature(vocabulary=vocab, add_eos=False),
+        "decoder_input_tokens": seqio.Feature(vocabulary=vocab, add_eos=False),
+        "decoder_loss_weights": seqio.Feature(vocabulary=vocab, add_eos=False),
+        "decoder_causal_attention": seqio.Feature(vocabulary=vocab, add_eos=False),
+        # All but the last stage of the preprocessing uses "targets" as the key,
+        # so this output feature is necessary. It is not marked required because
+        # the final preprocessor drops it.
+        "targets": seqio.Feature(vocabulary=vocab, required=False),
+    },
     metric_fns=[]
 )
